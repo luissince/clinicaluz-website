73e2ec6f4bc5d23bc74062cd849c9697f9748ff9
Ruber Flores <ruberfc@gmail.com>
2023-01-10 11:31:15 -0500

    Actualización del Dockerfile

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-docker stop nextapp && docker rm nextapp
+docker stop next-app && docker rm next-app
 
-docker image rm nextapp
+docker image rm next-app
 
-docker build -t nextapp .
+docker build -t next-app .
 
-docker run -d --name nextapp --net=my-net -p 3000:3000 nextapp
+docker run -d --name next-app --net=my-net -p 3000:3000 next-app
