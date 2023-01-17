24b42d250c2b9b8299fe54ca355ac25ff3ea0518
Luis Lara <alexander_dx_10@hotmail.com>
2023-01-17 09:21:48 -0500

    Completando cambios

--- components/card_deal.js
+++ components/card_deal.js
@@ -3,19 +3,24 @@ import Button from "./button";
 
 const CardDeal = () => (
   <section className="relative overflow-hidden bg-white w-full">
-    <div className="container max-w-screen-2xl mx-auto px-12">
-      <div className="mx-auto max-w-2xl lg:mx-0">
+    <div className="container max-w-screen-2xl mx-auto px-12 py-12">
+
+      <div className="max-w-2xl">
         <h2 className="font-display text-4xl font-medium tracking-tighter text-blue-600 sm:text-5xl">
           Staff Médico
         </h2>
+
         <p className="mt-4 font-display text-2xl tracking-tight text-blue-900">
           Learn from the experts on the cutting-edge of deception at the most sinister companies.
         </p>
       </div>
+
       <div className="mt-14 grid grid-cols-1 items-start gap-y-8 gap-x-8 sm:mt-16 sm:gap-y-16 lg:mt-24 lg:grid-cols-4">
         <div className="relative -mx-4 flex overflow-x-auto pb-4 sm:mx-0 sm:block sm:overflow-visible sm:pb-0">
-          <div classname="absolute bottom-0 top-2 left-0.5 hidden w-px bg-slate-200 lg:block"></div>
+          <div className="absolute bottom-0 top-2 left-0.5 hidden w-px bg-slate-200 lg:block"></div>
+         
           <div className="grid auto-cols-auto grid-flow-col justify-start gap-x-8 gap-y-10 whitespace-nowrap px-4 sm:mx-auto sm:max-w-2xl sm:grid-cols-3 sm:px-0 sm:text-center lg:grid-flow-row lg:grid-cols-1 lg:text-left" role="tablist" aria-orientation="vertical">
+           
             <div className="relative lg:pl-8">
               <svg aria-hidden="true" viewBox="0 0 6 6" className="absolute top-[0.5625rem] left-[-0.5px] hidden h-1.5 w-1.5 overflow-visible lg:block fill-blue-600 stroke-blue-600">
                 <path d="M3 0L6 3L3 6L0 3Z" stroke-width="2" stroke-linejoin="round"></path>
@@ -30,6 +35,7 @@ const CardDeal = () => (
                 <time datetime="2022-04-04" className="mt-1.5 block text-2xl font-semibold tracking-tight text-blue-900">April 4</time>
               </div>
             </div>
+
             <div className="relative lg:pl-8">
               <svg aria-hidden="true" viewBox="0 0 6 6" className="absolute top-[0.5625rem] left-[-0.5px] hidden h-1.5 w-1.5 overflow-visible lg:block fill-transparent stroke-slate-400">
                 <path d="M3 0L6 3L3 6L0 3Z" stroke-width="2" stroke-linejoin="round"></path>
@@ -44,6 +50,7 @@ const CardDeal = () => (
                 <time datetime="2022-04-05" class="mt-1.5 block text-2xl font-semibold tracking-tight text-blue-900">April 5</time>
               </div>
             </div>
+
             <div className="relative lg:pl-8">
               <svg aria-hidden="true" viewBox="0 0 6 6" className="absolute top-[0.5625rem] left-[-0.5px] hidden h-1.5 w-1.5 overflow-visible lg:block fill-transparent stroke-slate-400">
                 <path d="M3 0L6 3L3 6L0 3Z" stroke-width="2" stroke-linejoin="round"></path>
@@ -58,25 +65,26 @@ const CardDeal = () => (
                 <time datetime="2022-04-06" class="mt-1.5 block text-2xl font-semibold tracking-tight text-blue-900">April 6</time>
               </div>
             </div>
+
           </div>
+
         </div>
+
         <div className="lg:col-span-3">
           <div className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 sm:gap-y-16 md:grid-cols-3 [&:not(:focus-visible)]:focus:outline-none" role="tabpanel" tabindex="0" data-headlessui-state="selected" aria-labelledby="headlessui-tabs-tab-:r1:">
 
             <div>
-              <div className="group relative h-[17.5rem] transform overflow-hidden rounded-3xl">
+              <div className="group relative h-[20rem] transform overflow-hidden rounded-3xl">
                 <div className="absolute top-0 left-0 right-4 bottom-6 rounded-3xl border transition duration-300 group-hover:scale-95 xl:right-6 border-blue-300">
                 </div>
-                <div className="absolute inset-0" >
 
+                <div className="absolute inset-0" >
                   <img
                     src="https://keynote.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fsteven-mchail.4e94472e.jpg&w=384&q=75"
                     sizes="(min-width: 1280px) 17.5rem, (min-width: 1024px) 25vw, (min-width: 768px) 33vw, (min-width: 640px) 50vw, 100vw"
                     width="1120" height="560"
                     decoding="async" data-nimg="1"
-                    className="absolute inset-0 h-full w-full object-cover transition duration-300 group-hover:scale-110 svg1"
-                    style={{ "color": "transparent" }} />
-
+                    className="absolute inset-0 h-full w-full object-cover transition duration-300 group-hover:scale-110 svg1 text-transparent" />
                 </div>
                 <svg viewBox="0 0 1 1">
                   <defs>
@@ -91,7 +99,7 @@ const CardDeal = () => (
             </div>
 
             <div>
-              <div className="group relative h-[17.5rem] transform overflow-hidden rounded-3xl">
+              <div className="group relative h-[20rem] transform overflow-hidden rounded-3xl">
                 <div className="absolute top-0 left-0 right-4 bottom-6 rounded-3xl border transition duration-300 group-hover:scale-95 xl:right-6 border-indigo-300"></div>
                 <div className="absolute inset-0">
                   <img alt=""
@@ -113,8 +121,9 @@ const CardDeal = () => (
               <h3 className="mt-8 font-display text-xl font-bold tracking-tight text-slate-900">Jaquelin Isch</h3>
               <p className="mt-1 text-base tracking-tight text-slate-500">UX Design at InGen</p>
             </div>
+
             <div>
-              <div className="group relative h-[17.5rem] transform overflow-hidden rounded-3xl">
+              <div className="group relative h-[20rem] transform overflow-hidden rounded-3xl">
                 <div className="absolute top-0 left-0 right-4 bottom-6 rounded-3xl border transition duration-300 group-hover:scale-95 xl:right-6 border-sky-300"></div>
                 <div className="absolute inset-0">
                   <img alt=""
@@ -135,16 +144,17 @@ const CardDeal = () => (
               <h3 className="mt-8 font-display text-xl font-bold tracking-tight text-slate-900">Dianne Guilianelli</h3>
               <p className="mt-1 text-base tracking-tight text-slate-500">General Manager at Initech</p>
             </div>
+
             <div>
-              <div className="group relative h-[17.5rem] transform overflow-hidden rounded-3xl">
+              <div className="group relative h-[20rem]  transform overflow-hidden rounded-3xl">
                 <div className="absolute top-0 left-0 right-4 bottom-6 rounded-3xl border transition duration-300 group-hover:scale-95 xl:right-6 border-blue-300"></div>
                 <div className="absolute inset-0">
-                  <img alt="" 
-                  sizes="(min-width: 1280px) 17.5rem, (min-width: 1024px) 25vw, (min-width: 768px) 33vw, (min-width: 640px) 50vw, 100vw" 
-                  src="https://keynote.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fronni-cantadore.0d04b7fe.jpg&w=3840&q=75" 
-                  width="1120" height="560" 
-                  decoding="async" data-nimg="1" 
-                  className="absolute inset-0 h-full w-full object-cover transition duration-300 group-hover:scale-110 svg1" />
+                  <img alt=""
+                    sizes="(min-width: 1280px) 17.5rem, (min-width: 1024px) 25vw, (min-width: 768px) 33vw, (min-width: 640px) 50vw, 100vw"
+                    src="https://keynote.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fronni-cantadore.0d04b7fe.jpg&w=3840&q=75"
+                    width="1120" height="560"
+                    decoding="async" data-nimg="1"
+                    className="absolute inset-0 h-full w-full object-cover transition duration-300 group-hover:scale-110 svg1" />
                   <svg viewBox="0 0 1 1">
                     <defs>
                       <clipPath clipPathUnits="objectBoundingBox" id="imgSvg1">
@@ -157,16 +167,17 @@ const CardDeal = () => (
               <h3 className="mt-8 font-display text-xl font-bold tracking-tight text-slate-900">Ronni Cantadore</h3>
               <p className="mt-1 text-base tracking-tight text-slate-500">Design Engineer at Weyland-Yutani</p>
             </div>
+
             <div>
-              <div className="group relative h-[17.5rem] transform overflow-hidden rounded-3xl">
+              <div className="group relative h-[20rem]  transform overflow-hidden rounded-3xl">
                 <div className="absolute top-0 left-0 right-4 bottom-6 rounded-3xl border transition duration-300 group-hover:scale-95 xl:right-6 border-indigo-300"></div>
                 <div className="absolute inset-0">
-                  <img alt="" 
-                  sizes="(min-width: 1280px) 17.5rem, (min-width: 1024px) 25vw, (min-width: 768px) 33vw, (min-width: 640px) 50vw, 100vw" 
-                  src="https://keynote.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ferhart-cockrin.3697fef6.jpg&w=3840&q=75" 
-                  width="1120" height="560" 
-                  decoding="async" data-nimg="1" 
-                  className="absolute inset-0 h-full w-full object-cover transition duration-300 group-hover:scale-110 svg2" />
+                  <img alt=""
+                    sizes="(min-width: 1280px) 17.5rem, (min-width: 1024px) 25vw, (min-width: 768px) 33vw, (min-width: 640px) 50vw, 100vw"
+                    src="https://keynote.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ferhart-cockrin.3697fef6.jpg&w=3840&q=75"
+                    width="1120" height="560"
+                    decoding="async" data-nimg="1"
+                    className="absolute inset-0 h-full w-full object-cover transition duration-300 group-hover:scale-110 svg2" />
                   <svg viewBox="0 0 1 1">
                     <defs>
                       <clipPath clipPathUnits="objectBoundingBox" id="imgSvg2">
@@ -179,16 +190,17 @@ const CardDeal = () => (
               <h3 className="mt-8 font-display text-xl font-bold tracking-tight text-slate-900">Erhart Cockrin</h3>
               <p className="mt-1 text-base tracking-tight text-slate-500">Product Lead at Cyberdyne Systems</p>
             </div>
+
             <div>
-              <div className="group relative h-[17.5rem] transform overflow-hidden rounded-3xl">
+              <div className="group relative h-[20rem]  transform overflow-hidden rounded-3xl">
                 <div className="absolute top-0 left-0 right-4 bottom-6 rounded-3xl border transition duration-300 group-hover:scale-95 xl:right-6 border-sky-300"></div>
                 <div className="absolute inset-0">
-                  <img alt="" 
-                  sizes="(min-width: 1280px) 17.5rem, (min-width: 1024px) 25vw, (min-width: 768px) 33vw, (min-width: 640px) 50vw, 100vw" 
-                  src="https://keynote.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fparker-johnson.3f6d7276.jpg&w=3840&q=75" 
-                  width="1120" height="560" 
-                  decoding="async" data-nimg="1" 
-                  className="absolute inset-0 h-full w-full object-cover transition duration-300 group-hover:scale-110 svg3" />
+                  <img alt=""
+                    sizes="(min-width: 1280px) 17.5rem, (min-width: 1024px) 25vw, (min-width: 768px) 33vw, (min-width: 640px) 50vw, 100vw"
+                    src="https://keynote.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fparker-johnson.3f6d7276.jpg&w=3840&q=75"
+                    width="1120" height="560"
+                    decoding="async" data-nimg="1"
+                    className="absolute inset-0 h-full w-full object-cover transition duration-300 group-hover:scale-110 svg3" />
                   <svg viewBox="0 0 1 1">
                     <defs>
                       <clipPath clipPathUnits="objectBoundingBox" id="imgSvg3">
@@ -201,9 +213,11 @@ const CardDeal = () => (
               <h3 className="mt-8 font-display text-xl font-bold tracking-tight text-slate-900">Parker Johnson</h3>
               <p className="mt-1 text-base tracking-tight text-slate-500">UI Designer at MomCorp</p>
             </div>
+
           </div>
         </div>
       </div>
+
     </div>
   </section>
 );
