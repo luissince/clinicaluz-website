9f2f286f3d023598cccc8880e15b84383b1b1937
Luis Lara <alexander_dx_10@hotmail.com>
2023-01-27 15:12:53 -0500

    Guardando cambios

--- components/clients.js
+++ components/clients.js
@@ -1,10 +1,10 @@
-import { clients } from "../constants";
+import { navigation } from "../constants";
 
 const Clients = () => (
   <section className={`w-full my-12`}>
     <div className="container max-w-screen-2xl mx-auto ss:px-12 px-6">
       <div className={`flex justify-center items-center flex-wrap`}>
-        {clients.map((client) => (
+        {navigation.map((client) => (
           <div key={client.id} className={`flex-1 flex justify-center items-center sm:min-w-[192px] min-w-[120px] m-5`}>
             <img src={client.logo} alt="client_logo" className="sm:w-[192px] w-[100px] object-contain" />
           </div>
