42a54d3d5b95d8923daffcc2fa21ce446c28201b
Luis Lara <xanderls25@icloud.com>
2023-07-16 16:02:20 -0500

    Cambiando el net del docker

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker image rm next-app
 
 docker build -t next-app .
 
-docker run -d --name next-app --net=my-net -p 3000:3000 next-app
+docker run -d --name next-app --net=luis -p 3000:3000 next-app
